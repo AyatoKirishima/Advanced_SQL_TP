1134dc4ae9ee4ede1591a823ac42ccb5aac19de9
115AKIRA <84973711+115AKIRA@users.noreply.github.com>
2021-06-01 10:12:29 +0200

    Add files via upload

---
+++ TP_Blanc/sp_calcul_entree.sql
@@ -0,0 +1,23 @@
+CREATE DEFINER=`baggioko1u_appli`@`%` PROCEDURE `sp_calcul_entree`(IN entid INT, OUT prix DOUBLE)
+BEGIN
+	DECLARE id INT;
+    DECLARE hdeb, hfin INT;
+    DECLARE jdeb, jfin INT;
+    DECLARE total DOUBLE;
+    DECLARE duree INT;
+    
+    SET id := entid;
+    
+    SET hdeb := ( SELECT heure_in FROM ENTREE WHERE id = ent_id );
+    
+    SET hfin := ( SELECT heure_out FROM ENTREE WHERE id = ent_id );
+    
+    SET jdeb := ( SELECT jour_in FROM ENTREE WHERE id = ent_id );
+    
+    SET jfin := ( SELECT jour_out FROM ENTREE WHERE id = ent_id );
+    
+    
+	CALL sp_calcul_prix(jdeb, hdeb, jfin, hfin, @prix);
+    
+
+END
